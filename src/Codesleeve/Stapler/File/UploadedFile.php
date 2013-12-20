643cf4f5f998bab50892f88baa5193468deabcee
Travis Bennett <tandrewbennett@hotmail.com>
2013-12-19 22:38:28 -0600

    Remove Config class dependency from UploadedFile.
    Removed the usage of Laravel's Config facade from the UploadedFile
    class.  Valid image MIME types are now stored as an array directly
    inside the UploadedFile class.

--- src/Codesleeve/Stapler/File/UploadedFile.php
+++ src/Codesleeve/Stapler/File/UploadedFile.php
@@ -1,9 +1,24 @@
 <?php namespace Codesleeve\Stapler\File;
 
-use Illuminate\Support\Facades\Config as Config;
-
 class UploadedFile extends \Symfony\Component\HttpFoundation\File\UploadedFile
 {
+	/**
+	 * An array of key value pairs for valid image
+	 * extensions and their associated MIME types.
+	 * 
+	 * @var array
+	 */
+	protected $imageMimes = [
+		'bmp'   => 'image/bmp',
+		'gif'   => 'image/gif',
+		'jpeg'  => array('image/jpeg', 'image/pjpeg'),
+		'jpg'   => array('image/jpeg', 'image/pjpeg'),
+		'jpe'   => array('image/jpeg', 'image/pjpeg'),
+		'png'   => 'image/png',
+		'tiff'  => 'image/tiff',
+		'tif'   => 'image/tiff',
+	];
+
 	/**
 	 * Utility method for detecing whether a given file upload is an image.
 	 *
@@ -11,15 +26,14 @@ class UploadedFile extends \Symfony\Component\HttpFoundation\File\UploadedFile
 	 */
 	public function isImage()
 	{
-		$extensions = ['jpg', 'jpeg', 'gif', 'png'];
-		$mimes = Config::get('stapler::mimes');
 		$mime = $this->getMimeType();
 		
-		// The MIME configuration file contains an array of file extensions and
-		// their associated MIME types. We will loop through each extension and look for the MIME type.
-		foreach ($extensions as $extension)
+		// The $imageMimes property contains an array of file extensions and
+		// their associated MIME types. We will loop through them and look for 
+		// the MIME type of the current UploadedFile.
+		foreach ($this->imageMimes as $imageMime)
 		{
-			if (isset($mimes[$extension]) and in_array($mime, (array) $mimes[$extension]))
+			if (in_array($mime, (array) $imageMime))
 			{
 				return true;
 			}
@@ -32,25 +46,24 @@ class UploadedFile extends \Symfony\Component\HttpFoundation\File\UploadedFile
      * Returns an informative upload error message.
      *
      * @param int $code The error code returned by an upload attempt
-     *
      * @return string The error message regarding the specified error code
      */
     public function getErrorMessage($errorCode)
     {
-        static $errors = array(
-            UPLOAD_ERR_INI_SIZE   => 'The file "%s" exceeds your upload_max_filesize ini directive (limit is %d kb).',
-            UPLOAD_ERR_FORM_SIZE  => 'The file "%s" exceeds the upload limit defined in your form.',
-            UPLOAD_ERR_PARTIAL    => 'The file "%s" was only partially uploaded.',
-            UPLOAD_ERR_NO_FILE    => 'No file was uploaded.',
-            UPLOAD_ERR_CANT_WRITE => 'The file "%s" could not be written on disk.',
-            UPLOAD_ERR_NO_TMP_DIR => 'File could not be uploaded: missing temporary directory.',
-            UPLOAD_ERR_EXTENSION  => 'File upload was stopped by a php extension.',
-        );
+		static $errors = [
+			UPLOAD_ERR_INI_SIZE   => 'The file "%s" exceeds your upload_max_filesize ini directive (limit is %d kb).',
+			UPLOAD_ERR_FORM_SIZE  => 'The file "%s" exceeds the upload limit defined in your form.',
+			UPLOAD_ERR_PARTIAL    => 'The file "%s" was only partially uploaded.',
+			UPLOAD_ERR_NO_FILE    => 'No file was uploaded.',
+			UPLOAD_ERR_CANT_WRITE => 'The file "%s" could not be written on disk.',
+			UPLOAD_ERR_NO_TMP_DIR => 'File could not be uploaded: missing temporary directory.',
+			UPLOAD_ERR_EXTENSION  => 'File upload was stopped by a php extension.',
+		];
 
-        $maxFilesize = $errorCode === UPLOAD_ERR_INI_SIZE ? self::getMaxFilesize() / 1024 : 0;
-        $message = isset($errors[$errorCode]) ? $errors[$errorCode] : 'The file "%s" was not uploaded due to an unknown error.';
+		$maxFilesize = $errorCode === UPLOAD_ERR_INI_SIZE ? self::getMaxFilesize() / 1024 : 0;
+		$message = isset($errors[$errorCode]) ? $errors[$errorCode] : 'The file "%s" was not uploaded due to an unknown error.';
 
-       return sprintf($message, $this->getClientOriginalName(), $maxFilesize);
+		return sprintf($message, $this->getClientOriginalName(), $maxFilesize);
     }
 
 }
