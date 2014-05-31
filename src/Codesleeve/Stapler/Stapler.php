f9790ac6dc2f57321463518ff3059a9ef5249c73
Travis Bennett <tandrewbennett@hotmail.com>
2014-05-31 00:07:20 -0500

    Remove singleton methods from Stapler class.
    Removed the singleton override methods from the Stapler class since it's
    not longer a singleton implmentation.

--- src/Codesleeve/Stapler/Stapler.php
+++ src/Codesleeve/Stapler/Stapler.php
@@ -216,32 +216,4 @@ class Stapler
     {
         return S3Client::factory($attachedFile->s3_client_config);
     }
-
-    /**
-     * Protected constructor to prevent creating a new instance of the
-     * *Singleton* via the `new` operator from outside of this class.
-     */
-    protected function __construct()
-    {
-    }
-
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * *Singleton* instance.
-     *
-     * @return void
-     */
-    private function __clone()
-    {
-    }
-
-    /**
-     * Private unserialize method to prevent unserializing of the *Singleton*
-     * instance.
-     *
-     * @return void
-     */
-    private function __wakeup()
-    {
-    }
 }
