d2a51d26d1e4b1494890e60f854aa52b53f8d766
Travis Bennett <tandrewbennett@hotmail.com>
2016-05-13 16:37:22 +0000

    Refactor the underlying storage structure to work with flysystem.

---
+++ src/Storage/CloudStorage.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Codesleeve\Stapler\Storage;
+
+use League\Flysystem\FilesystemInterface;
+use Codesleeve\Stapler\Attachment;
+
+abstract class CloudStorage
+{
+    /**
+     * The current attachedFile object being processed.
+     *
+     * @var \Codesleeve\Stapler\Attachment
+     */
+    public $attachedFile;
+
+    /**
+     * The AWS S3Client instance.
+     *
+     * @var FilesystemInterface
+     */
+    protected $filesystem;
+
+    /**
+     * Constructor method.
+     *
+     * @param Attachment           $attachedFile
+     * @param FilesystemInterface  $filesystem
+     */
+    public function __construct(Attachment $attachedFile, FilesystemInterface $filesystem)
+    {
+        $this->attachedFile = $attachedFile;
+        $this->filesystem = $filesystem;
+    }
+
+    /**
+     * Return the key the uploaded file object is stored under within a bucket.
+     *
+     * @param string $styleName
+     *
+     * @return string
+     */
+    public function path($styleName)
+    {
+        $path = $this->attachedFile->path;
+        $interpolator = $this->attachedFile->getInterpolator();
+
+        return $interpolator->interpolate($path, $this->attachedFile, $styleName);
+    }
+
+    /**
+     * Remove an attached file.
+     *
+     * @param array $filePaths
+     */
+    public function remove(array $filePaths)
+    {
+        if ($filePaths) {
+            foreach ($filePaths as $filePath) {
+                $this->filesystem->delete($filePath);
+            }
+        }
+    }
+
+    /**
+     * Return the url for a file upload.
+     *
+     * @param string $styleName
+     *
+     * @return string|void
+     */
+    abstract public function url($styleName);
+
+    /**
+     * Move an uploaded file to its intended destination.
+     *
+     * @param string $file
+     * @param string $filePath
+     */
+    abstract public function move($file, $filePath);
+}
