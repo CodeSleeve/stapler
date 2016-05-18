1657bda77e808dee272d206da20f78839ed79514
Travis Bennett <tandrewbennett@hotmail.com>
2016-05-18 05:02:32 +0000

    Refactor tests back to green.

--- tests/Codesleeve/Stapler/Factories/StorageTest.php
+++ tests/Codesleeve/Stapler/Factories/StorageTest.php
@@ -99,10 +99,13 @@ class StorageTest extends PHPUnit_Framework_TestCase
             'storage' => 's3',
             'styles' => [],
             's3_client_config' => [
-                'key' => '',
-                'secret' => '',
+                'credentials' => [
+                    'key' => '',
+                    'secret' => ''
+                ],
                 'region' => '',
                 'scheme' => 'http',
+                'version' => 'latest'
             ],
         ]);
         $attachment->setConfig($attachmentConfig);
