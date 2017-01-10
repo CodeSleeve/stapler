c8fe7caa43e93c22411ea6764bbc2b75cd674ef8
Travis Bennett <tandrewbennett@hotmail.com>
2017-01-10 17:01:51 +0000

    Add tests to make sure :hash interpolations will be properly interpolated.

--- tests/Codesleeve/Stapler/InterpolatorTest.php
+++ tests/Codesleeve/Stapler/InterpolatorTest.php
@@ -27,7 +27,7 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->interpolator = $this->interpolator ?: new Interpolator();
-        $this->attachment = $this->attachment ?: $this->build_mock_attachment($this->interpolator);
+        $this->attachment = $this->attachment ?: $this->buildMockAttachment($this->interpolator);
     }
 
     /**
@@ -69,18 +69,48 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test that the interpolator will correctly interpolate a string when
-     * using an id partition.
+     * Test that the interpolator will correctly interpolate a string containing an :id_partition interpolation.
      *
      * @test
      */
-    public function it_should_be_able_to_interpolate_a_string_using_an_id_partition()
+    public function it_should_be_able_to_interpolate_a_string_containing_an_id_partition()
     {
         $input = '/system/:class/:attachment/:id_partition/:style/:filename';
 
         $interpolatedString = $this->interpolator->interpolate($input, $this->attachment, 'thumbnail');
 
         $this->assertEquals('/system/test_model/photos/000/000/001/thumbnail/test.jpg', $interpolatedString);
+    }
+
+    /**
+     * Test that the interpolator will correctly interpolate a string containing a :hash interpolation.
+     *
+     * @test
+     */
+    public function it_should_be_able_to_interpolate_a_string_containing_a_hash()
+    {
+        $attachmentConfig = new AttachmentConfig('photo', ['styles' => [], 'default_style' => 'original', 'hash_secret' => 'abfa04a42c94f58d17a509bccb2276d2f2e1718e23de5f0ff4bc93b4c922c2dbd23f81b31a7932fbf4424c95f14e055639d2376f8b3cb40ebf91ea4682197645']);
+        $attachment = $this->buildMockAttachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel', $attachmentConfig);
+        $input = '/system/:class_name/:attachment/:id_partition/:style/:hash';
+        $interpolatedString = $this->interpolator->interpolate($input, $attachment, 'thumbnail');
+
+        $this->assertEquals('/system/test_model/photos/000/000/001/thumbnail/fcab8328c8194101054eb52fb1e0189b26e0faccb68fe24fa73625ab4bc44c5e', $interpolatedString);
+    }
+
+    /**
+     * Test that the interpolator will throw an exception if it attempts to interpolate a string with
+     * a :hash interpolation but no hash_string option has been set.
+     *
+     * @expectedException \Codesleeve\Stapler\Exceptions\InvalidAttachmentConfigurationException
+     * @test
+     */
+    public function it_should_throw_an_exception_if_it_interpolates_a_string_containing_a_hash()
+    {
+        $attachment = $this->buildMockAttachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel');
+        $input = '/system/:class_name/:attachment/:id_partition/:style/:hash';
+        $interpolatedString = $this->interpolator->interpolate($input, $attachment, 'thumbnail');
+
+        $this->assertEquals('/system/test_model/photos/000/000/001/thumbnail/fcab8328c8194101054eb52fb1e0189b26e0faccb68fe24fa73625ab4bc44c5e', $interpolatedString);
     }
 
     /**
@@ -91,7 +121,7 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
      */
     public function it_should_be_able_to_interpolate_a_string_using_a_class_name()
     {
-        $attachment = $this->build_mock_attachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel');
+        $attachment = $this->buildMockAttachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel');
         $input = '/system/:class_name/:attachment/:id_partition/:style/:filename';
         $interpolatedString = $this->interpolator->interpolate($input, $attachment, 'thumbnail');
 
@@ -99,14 +129,13 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test that the interpolator will correctly interpolate a string when
-     * using a namespace.
+     * Test that the interpolator will correctly interpolate a string when a :namespace interpolation.
      *
      * @test
      */
     public function it_should_be_able_to_interpolate_a_string_using_a_namespace()
     {
-        $attachment = $this->build_mock_attachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel');
+        $attachment = $this->buildMockAttachment($this->interpolator, 'Foo\\Faz\\Baz\\TestModel');
         $input = '/system/:namespace/:attachment/:id_partition/:style/:filename';
         $interpolatedString = $this->interpolator->interpolate($input, $attachment, 'thumbnail');
 
@@ -114,14 +143,13 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test that the interpolator will correctly interpolate a string when
-     * using a namespace and class name.
+     * Test that the interpolator will correctly interpolate a string with a :class_name interpolation.
      *
      * @test
      */
     public function it_should_be_able_to_interpolate_a_string_using_a_namespace_and_class_name()
     {
-        $attachment = $this->build_mock_attachment($this->interpolator, 'Foo\\Bar\\Baz\\TestModel');
+        $attachment = $this->buildMockAttachment($this->interpolator, 'Foo\\Bar\\Baz\\TestModel');
         $input = '/system/:namespace/:class_name/:attachment/:id_partition/:style/:filename';
         $interpolatedString = $this->interpolator->interpolate($input, $attachment, 'thumbnail');
 
@@ -150,14 +178,18 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
     /**
      * Build a mock attachment object.
      *
-     * @param  \Codesleeve\Stapler\Interpolator
-     *
-     * @return \Codesleeve\Stapler\Attachment
+     * @param  Interpolator     $interpolator
+     * @param  string           $className
+     * @param  AttachmentConfig $attachmentConfig
+     * @return void
      */
-    protected function build_mock_attachment($interpolator, $className = 'TestModel')
+    protected function buildMockAttachment(Interpolator $interpolator, string $className = 'TestModel', AttachmentConfig $attachmentConfig = null)
     {
-        $instance = $this->build_mock_instance();
-        $attachmentConfig = new AttachmentConfig('photo', ['styles' => [], 'default_style' => 'original']);
+        if (!$attachmentConfig) {
+            $attachmentConfig = new AttachmentConfig('photo', ['styles' => [], 'default_style' => 'original']);
+        }
+
+        $instance = $this->buildMockInstance();
         $imagine = m::mock('Imagine\Image\ImagineInterface');
         $resizer = new \Codesleeve\Stapler\File\Image\Resizer($imagine);
         $dispatcher = new \Codesleeve\Stapler\NativeEventDispatcher;
@@ -174,7 +206,7 @@ class InterpolatorTest extends PHPUnit_Framework_TestCase
      *
      * @return mixed
      */
-    protected function build_mock_instance()
+    protected function buildMockInstance()
     {
         $instance = m::mock('Codesleeve\Stapler\ORM\StaplerableInterface');
         $instance->shouldReceive('getKey')->andReturn(1);
