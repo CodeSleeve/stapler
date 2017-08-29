976e9edde7283c312308d21d0647ff33c6be0209
John M <john@ews-network.net>
2017-08-29 16:34:00 +0000

    Added condition for AWS IAM Profile support and not to require AWS Credentials in the defines

--- src/Validator.php
+++ src/Validator.php
@@ -46,12 +46,23 @@ class Validator implements ValidatorInterface
             throw new Exceptions\InvalidUrlOptionException('Invalid Path: a bucket is required for s3 storage.', 1);
         }
 
-        if (!$options['s3_client_config']['secret']) {
-            throw new Exceptions\InvalidUrlOptionException('Invalid Path: a secret is required for s3 storage.', 1);
-        }
+	/*
+	*
+	* On an AWS EC2 instance, checks if security credentials are available. If not, requires the variables.
+	* If so, the AWS PHP SDK will use those credentials to handle all of the API calls
+	*
+	*/
+	$file = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/';
+	$file_headers = @get_headers($file);
+	if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
 
-        if (!$options['s3_client_config']['key']) {
-            throw new Exceptions\InvalidUrlOptionException('Invalid Path: a key is required for s3 storage.', 1);
-        }
+		if (!$options['s3_client_config']['secret']) {
+        	    throw new Exceptions\InvalidUrlOptionException('Invalid Path: a secret is required for s3 storage.', 1);
+        	    }
+
+	        if (!$options['s3_client_config']['key']) {
+            	throw new Exceptions\InvalidUrlOptionException('Invalid Path: a key is required for s3 storage.', 1);
+        	}
+	}
     }
 }
